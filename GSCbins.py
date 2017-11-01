4e310bf0978af37ec6ac6801d5725e5193ee047c
tmressler <tmressler@gmail.com>
2017-10-31 17:35:19 -0700

    Constraints List and README
    Fixed a constraints list bug and updated the README.

--- GSCbins.py
+++ GSCbins.py
@@ -6,67 +6,66 @@ Created on Mon Oct 30 13:57:31 2017
 """
         
 class Language():    
-    def __init__(self, name, token, constraints):
-        self.name = name
+    def __init__(self, token, description, set_of_constraints):
         self.token = token
-        self.language_constraints = [constraints]
+        self.description = description
+        self.constraints = [set_of_constraints]
         
     # adds a set of constraints to Language
-    def __lshift__(self, constraints):
-        self.language_constraints.append(constraints)
+    def __lshift__(self, set_of_constraints):
+        self.constraints.append(set_of_constraints)
         
     # returns True if a set of costraints is in Language
-    def __contains__(self, constraints):
-        if constraints in self.language_constraints:
+    def __contains__(self, set_of_constraints):
+        if set_of_constraints in self.constraints:
             return True
         else:
             return False
     
     # returns the number of constraints
-    def __len__(self):
-        return len(self.language_constraints)
+    def count(self):
+        return len(self.constraints)
         
-    # prints a list of all sets of contraints
-    def contents(self):
-        print(self.language_constraints)
-     
 class Bin():
     def __init__(self):
-        self.bin_languages = []
+        self.languages = []
         
     # adds a language to a bin
-    def __lshift__(self, language):
+    def __lshift__(self, new_language):
         # checks if language token is already in bin
-        for bin_language in self.bin_languages:
+        for language in self.languages:
             # if token matches, add the constraints to token
-            if language.token == bin_language.token:
-                bin_language << language.language_constraints
+            if new_language.token == language.token:
+                for set_of_constraints in new_language.constraints:
+                    language << set_of_constraints
                 print("Constraints added")
                 return
-        
+
         # addes language to bin
-        self.bin_languages.append(language)
+        self.languages.append(new_language)
         print("Language binned")
       
     # searchs a bin for a language token
     def __contains__(self, token):
-        for language in self.bin_languages:
+        for language in self.languages:
             if token == language.token:
                 return True
         return False
+    
+    # returns the number of languages in a bin
+    def count(self):
+        return len(self.languages)
        
-    # prints a list of language tokens in a bin
+    # returns a list of language tokens in a bin
     def tokens_list(self):
-        for language in self.bin_languages:
-            print(language.token)
+        tokens_list = []
+        for language in self.languages:
+            tokens_list.append(language.token)
+        return tokens_list
        
-    # prints the contents of a bin
-    def contents(self):
-        print(self.bin_languages)
-     
     # returns the address of the token argument
     def token(self, token):
-        for language in self.bin_languages:
+        for language in self.languages:
             if token == language.token:
                 return language
         raise IndexError("token not in bin")
--- README.txt
+++ README.txt
@@ -2,7 +2,7 @@ GSCbins README
 
 class Language()
 To create a language:
-    language1 = Language("totally faithful", "LHO LHC SHO SHC LLC SLC LLO SLO", (-0.5, -0.5, -1, -1))
+    language1 = Language("LHO LHC SHO SHC LLC SLC LLO SLO", "totally faithful", (-0.5, -0.5, -1, -1))
 
 To add another set of constraints to a language:
     language1 << (-0.5, -0.6, -1, -1)
@@ -11,20 +11,34 @@ To see if a set of contraints is in a language:
     (-0.5, -0.6, -1, -1) in language1
 
 To count number of sets of constraints for a language:
-    len(language1)
+    language1.count()
     
-To print contents of a language:
-    language1.contents()
+To print a list of all of the sets of contraints in a language:
+    language1.constraints
+
+To print the description of a language:
+    language1.description
 
 class Bin()
 To create a bin:
-    good_bin = Bin('good')
+    good_bin = Bin()
 
 To add a language to a bin:
     good_bin << language1
     
+To see if a language token is in a bin:
+    "SHO SHC SHO SHC LLC SLC LLO SLO" in good_bin
+
+To count how many languages are in a bin:
+    good_bin.count()
+
 To return a list of language tokens in a bin:
     good_bin.tokens_list()
 
-To see if a language token is in a bin:
-    "SHO SHC SHO SHC LLC SLC LLO SLO" in good_bin
+To return the address of a specific language token in a bin:
+    good_bin.token("SHO SHC SHO SHC LLC SLC LLO SLO")
+
+    You can then do all Language operations on the token:
+        good_bin.token("SHO SHC SHO SHC LLC SLC LLO SLO") << (-0.5, -0.6, -1, -1)
+        good_bin.token("SHO SHC SHO SHC LLC SLC LLO SLO").constraints
+        etc.
