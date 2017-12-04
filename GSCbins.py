c16570a5e71c8a8c805809a83342d181ef47343a
tmressler <tmressler@gmail.com>
2017-12-04 15:55:05 -0800

    Update 3.0
    See richREADME.

--- GSCbins.py
+++ GSCbins.py
@@ -3,7 +3,7 @@
 Created on Mon Oct 30 13:57:31 2017
 
 @author: Karen Liou, Tim Ressler
-@version: 2.2
+@version: 3.0
     
 """
  
@@ -22,90 +22,75 @@ except ImportError:
     import pip
     pip.main(['install', 'scipy'])
     from scipy.spatial import ConvexHull, Delaunay
+    from scipy.spatial.qhull import QhullError
     print()
 try:
     import matplotlib.pyplot as plt
     import matplotlib.path as mpath
     import matplotlib.patches as mpatches
+    from mpl_toolkits.mplot3d import axes3d
 except ImportError:
     import pip
     pip.main(['install', 'matplotlib'])
     import matplotlib.pyplot as plt
+    import matplotlib.path as mpath
+    import matplotlib.patches as mpatches
+    from mpl_toolkits.mplot3d import axes3d
     print()
      
 class Language():        
-    def __init__(self, token, set_of_constraints, description=None):
+    def __init__(self, token, set_of_weights, description=None):
         self.token = token
-        self.constraints = numpy.array([set_of_constraints])
+        self.weights = numpy.array([set_of_weights])
         self.description = description
         
-    # adds a set of constraints to the language
-    def __lshift__(self, set_of_constraints):
-        self.constraints = numpy.vstack((self.constraints, set_of_constraints))
+    # adds a set of weights to the language
+    def __lshift__(self, set_of_weights):
+        self.weights = numpy.vstack((self.weights, set_of_weights))
 
-    # returns True if a set of constraints is in the convex hull of the language using Delauney triangulation
-    def __contains__(self, set_of_constraints):
-        return Delaunay(self.constraints).find_simplex(set_of_constraints) >= 0
+    # returns True if a set of weights is in the convex hull of the language using Delauney triangulation
+    def __contains__(self, set_of_weights):
+        return Delaunay(self.weights).find_simplex(set_of_weights) >= 0
         
-    # returns True if a set of costraints is in the language
-    def constraint(self, set_of_constraints):
-        if list(set_of_constraints) in self.constraints.data.tolist():
+    # returns True if a set of weights is a member of language.weights
+    def set_of_weights(self, set_of_weights):
+        if list(set_of_weights) in self.weights.data.tolist():
             return True
         else:
             return False
         
-    # returns a tuple of the minimum and maximum constraint(s)
-    def constraints_range(self, constraint=None):
-        min_constraints = numpy.amin(self.constraints, axis=0)
-        max_constraints = numpy.amax(self.constraints, axis=0)
+    # returns a tuple of the minimum and maximum weight(s)
+    def constraint_range(self, constraint=None):
+        min_constraints = numpy.amin(self.weights, axis=0)
+        max_constraints = numpy.amax(self.weights, axis=0)
         if constraint == None:
-            return (min_constraints, max_constraints)
+            return (min_constraints.tolist(), max_constraints.tolist())
         else:
             return (min_constraints[constraint], max_constraints[constraint])
      
-    # returns the number of constraints
-    def count(self):
-        return self.constraints.shape[0]   
+    # returns the number of sets of weights in the language
+    def count_weights(self):
+        return self.weights.shape[0]   
 
-    # checks if another language has sets of constraints in the language's convex hull
-    def language(self, language):
-        intersecting_constraints = []
-        for set_of_constraints in language.constraints.tolist():
-            if set_of_constraints in self:
-                intersecting_constraints.append(set_of_constraints)
-        return intersecting_constraints
+    # checks if another language has sets of weights in the language's convex hull
+    def weights_in(self, language):
+        intersecting_weights = []
+        for set_of_weights in self.weights.tolist():
+            if set_of_weights in language:
+                intersecting_weights.append(set_of_weights)
+        return intersecting_weights
             
-    # plots the language on two axes
-    def plot(self, first_constraint, second_constraint, alpha=0.1):
-        figure, axes = plt.subplots()
-        
-        # plots constraint points
-        axes.plot(self.constraints[:,first_constraint], self.constraints[:,second_constraint], 'o', label=self.token)
-        
-        # plots convex hull
-        try:
-            codes = [1]
-            vertices = []
-            for index in ConvexHull(self.constraints[:,[first_constraint, second_constraint]]).vertices:
-                codes.append(2)
-                vertices.append(self.constraints[index, [first_constraint, second_constraint]].tolist())
-            codes[len(codes) - 1] = 79
-            vertices.append(vertices[0])
-            path = mpath.Path(vertices, codes)
-            axes.add_patch(mpatches.PathPatch(path, alpha=alpha))
-            x, y = zip(*path.vertices)
-            line, = axes.plot(x, y, 'k-')
-            
-        except QhullError:
-            print("Note: language has no convex hull in these dimensions")
-    
+    # plots the language on two axes corresponding to two constraints
+    def plot(self, first_constraint, second_constraint, third_constraint=None, alpha=0.1):
+        master_plot(self, first_constraint, second_constraint, third_constraint, alpha, offset=0)
+
     # returns the verticies of the language's convex hull
     def vertices(self):
-        return self.constraints[ConvexHull(self.constraints).vertices]
+        return self.weights[ConvexHull(self.weights).vertices]
     
     # returns the volume of the language's convex hull
     def volume(self):
-        return ConvexHull(self.constraints).volume
+        return ConvexHull(self.weights).volume
        
 class Bin():
     def __init__(self):
@@ -115,9 +100,9 @@ class Bin():
     def __lshift__(self, new_language):
         # checks if language token is already in bin
         for language in self.languages:
-            # if token matches, add the constraints to token
+            # if token matches, add the weights to token
             if new_language.token == language.token:
-                language.constraints = numpy.concatenate((language.constraints, new_language.constraints), axis=0)
+                language.weights = numpy.concatenate((language.weights, new_language.weights), axis=0)
                 return
 
         # addes language to bin
@@ -130,35 +115,35 @@ class Bin():
                 return True
         return False
     
-    # returns the first language found with a set of constraints
-    def constraint_set(self, set_of_constraints):
+    # returns the first language found with a set of weights
+    def set_of_weights(self, set_of_weights):
         for language in self.languages:
-            if list(set_of_constraints) in language.constraints.data.tolist():
+            if list(set_of_weights) in language.weights.data.tolist():
                 return language
-        raise IndexError("set of constraints is not in bin")
+        raise IndexError("set of weights is not in bin")
         
-    # returns a list of tokens that are in a range, along with their constraints
+    # returns a list of tokens that are in a range, along with their weights
     def constraint_space(self, lower_limits, upper_limits):
         space_contents = []
         
         # check each language
         for language in self.languages:
-            intersecting_constraints = []
+            intersecting_weights = []
             
-            # check every set of constraints for a language
-            for set_of_constraints in language.constraints.data.tolist():
+            # check every set of weights for a language
+            for set_of_weights in language.weights.data.tolist():
                 constraint_number = 0
-                constraints_in_range = 0
+                weights_in_range = 0
                 
                 # check each constraint to be in range
-                for constraint in set_of_constraints:
+                for constraint in set_of_weights:
                     if constraint >= lower_limits[constraint_number] and constraint <= upper_limits[constraint_number]:
-                        constraints_in_range += 1
+                        weights_in_range += 1
                     constraint_number += 1
-                if constraints_in_range == len(set_of_constraints):
-                    intersecting_constraints.append(set_of_constraints)
+                if weights_in_range == len(set_of_weights):
+                    intersecting_weights.append(set_of_weights)
                     
-            space_contents.append([language.token, intersecting_constraints])
+            space_contents.append([language.token, intersecting_weights])
             
         return space_contents
                 
@@ -177,56 +162,22 @@ class Bin():
         for entry in f.readlines():
             entry = entry.split('|', 2)
             language = Language(entry[0], None, entry[1])
-            language.constraints = numpy.array(ast.literal_eval(entry[2].rstrip()))
+            language.weights = numpy.array(ast.literal_eval(entry[2].rstrip()))
             self << language
          
-    # plots the languages in the bin using two constraints
-    def plot_bin(self, first_constraint, second_constraint, alpha=0.1, offset=0.05):
-        self.plot_language(self.languages, first_constraint, second_constraint, alpha, offset)
-        
+    # plots the languages in the bin using two weights
+    def plot_bin(self, first_constraint, second_constraint, third_constraint=None, alpha=0.1, offset=0.05):
+        master_plot(self.languages, first_constraint, second_constraint, third_constraint, alpha, offset)
+  
     # plots one or more languages in the bin on a two dimensional graph
-    def plot_language(self, languages, first_constraint, second_constraint, alpha=0.1, offset=0.05):
-        error_tokens = []
-        figure, axes = plt.subplots()
-        counter = 0
-        
-        # if input is a Language, change to list format
-        if isinstance(languages, Language):
-            languages = [languages]
-            
-        for language in languages:
-            
-            # plots constrain points
-            axes.plot(language.constraints[:,first_constraint] + (counter * offset), language.constraints[:,second_constraint], 'o', label=language.token)
-            
-            # plots convex hull
-            try:
-                codes = [1]
-                vertices = []
-                for index in ConvexHull(language.constraints[:,[first_constraint, second_constraint]]).vertices:
-                    codes.append(2)
-                    vertices.append(language.constraints[index, [first_constraint, second_constraint]].tolist())
-                codes[len(codes) - 1] = 79
-                vertices.append(vertices[0])
-                path = mpath.Path(vertices, codes)
-                axes.add_patch(mpatches.PathPatch(path, facecolor='C'+str(counter % 10), alpha=alpha))
-                x, y = zip(*path.vertices)
-                line, = axes.plot(x, y, 'k-')
-                counter += 1
-            except QhullError:
-                error_tokens.append(language.token)
-                
-        # creates legend
-        axes.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize='x-large')
-        
-        if len(error_tokens) > 0:
-            print("Note - languages with no convex hull in these dimensions: " + ', '.join(error_tokens))
+    def plot_languages(self, languages, first_constraint, second_constraint, third_constraint=None, alpha=0.1, offset=0.05):
+        master_plot(languages, first_constraint, second_constraint, third_constraint, alpha, offset)
         
     # saves a bin to a text file
     def save(self, filename):
         f = open(filename, 'w')
         for language in self.languages:
-            f.write(language.token + '|' + str(language.description) + '|' + str(language.constraints.data.tolist()) + '\n')
+            f.write(language.token + '|' + str(language.description) + '|' + str(language.weights.data.tolist()) + '\n')
        
     # returns the address of the token argument
     def token(self, token):
@@ -288,31 +239,52 @@ def bin_language(language, good_bin, okay_bin, trash_bin):
         
     f.close()
     
-'''
-# FOR TESTING CONVEX HULLS
-def generate_cube():
-    cube = Language('cube', (0,0,0))
-    cube << (0,0,1)
-    cube << (0,1,0)
-    cube << (0,1,1)
-    cube << (1,0,0)
-    cube << (1,0,1)
-    cube << (1,1,0)
-    cube << (1,1,1)
-    
-    return cube
+def master_plot(languages, first_constraint, second_constraint, third_constraint, alpha, offset):
+    if isinstance(languages, Language):
+        languages = [languages]
+            
+    counter = 0
+        
+    if third_constraint == None:
+        error_tokens = []
+        figure, axes = plt.subplots()
+            
+        for language in languages:
+            
+            # plots constrain points
+            axes.plot(language.weights[:,first_constraint] + (counter * offset), language.weights[:,second_constraint], 'o', label=language.token)
+            
+            # plots convex hull using witchcraft
+            try:
+                codes = [1]
+                vertices = []
+                for index in ConvexHull(language.weights[:,[first_constraint, second_constraint]]).vertices:
+                    codes.append(2)
+                    vertices.append(language.weights[index, [first_constraint, second_constraint]].tolist())
+                    vertices[-1][0] += (counter * offset)
+                codes[len(codes) - 1] = 79
+                vertices.append(vertices[0])
+                path = mpath.Path(vertices, codes)
+                axes.add_patch(mpatches.PathPatch(path, facecolor='C'+str(counter % 10), alpha=alpha))
+                x, y = zip(*path.vertices)
+                line, = axes.plot(x, y, 'k-')
+                counter += 1
+            except QhullError:
+                error_tokens.append(language.token)
 
-def generate_points():
-    points = Language('points', (.1,.1,.1))
-    points << (-1,0,0)
-    points << (0,0,0)
-    points << (0.1, -0.3, -0.4)
-    
-    return points
-    
-cube = generate_cube()
-points = generate_points()
-the_bin = Bin()
-the_bin << cube
-the_bin << points
-'''
+        if len(error_tokens) > 0:
+            print("Languages with no convex hull in these dimensions: " + ', '.join(error_tokens))
+        
+    else:
+        figure = plt.figure()
+        axes = figure.add_subplot(111, projection='3d')
+        for language in languages:
+            axes.scatter(language.weights[:,first_constraint] + (counter * offset), language.weights[:,second_constraint], language.weights[:,third_constraint], label=language.token)
+
+        axes.set_zlabel("constraint " + str(third_constraint))
+     
+    axes.set_ylabel("constraint " + str(second_constraint))
+    axes.set_xlabel("constraint " + str(first_constraint))
+    chartBox = axes.get_position()
+    axes.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.8, chartBox.height])
+    axes.legend(loc='upper center', bbox_to_anchor=(1.2, 1), ncol=1)
